663f3f0f9614cdceb2f47cfd63b56e42cb924add
Robert Starr <rstarr@indeed.com>
2023-08-05 16:33:28 -0700

    removed .npmrc file.

--- app.js
+++ app.js
@@ -5,3 +5,30 @@ require('dotenv').config()
 
 // Creating express server
 const app = express();
+
+// Configuration
+const PORT = 3000;
+const HOST = "localhost";
+const {API_BASE_URL} = process.env;
+const {API_KEY_VALUE} = process.env;
+const API_SERVICE_URL = `${API_BASE_URL}?q=London&appid=${API_KEY_VALUE}`;
+
+// Logging the requests
+app.use(morgan("dev"));
+
+// Proxy Logic :  Proxy endpoints
+app.use(
+    "/weather",
+    createProxyMiddleware({
+        target: API_SERVICE_URL,
+        changeOrigin: true,
+        pathRewrite: {
+            "^/weather": "",
+        },
+    })
+);
+
+// Starting our Proxy server
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
